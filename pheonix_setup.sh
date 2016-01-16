ce2032bcb166e71a80c37d9beec589f88a13fc68
David Laprade <david.laprade@gmail.com>
2016-01-16 10:41:19 -0500

    Add postgresql setup for phoenix installation

--- pheonix_setup.sh
+++ pheonix_setup.sh
@@ -8,6 +8,21 @@ brew install elixir
 mix local.hex
 mix archive.install https://github.com/phoenixframework/phoenix/releases/download/v1.1.2/phoenix_new-1.1.2.ez
 
+# http://exponential.io/blog/2015/02/21/install-postgresql-on-mac-os-x-via-brew/
+echo "Installing postgresql..."
+brew uninstall --force postgresql
+brew install postgresql
+
+echo "Creating first postgres database..."
+initdb /usr/local/var/postgres/ -E utf8
+createdb `whoami`
+
+echo "Starting postgres server..."
+pg_ctl -D /usr/local/var/postgres/ -l logfile start
+
+echo "Logging into postgres for the first time. Enter the following command: `CREATE ROLE postgres LOGIN CREATEDB`. Then exit with `CTRL + d`"
+psql
+
 echo "Erlang, Elixir, and Pheonix installed! If this worked properly, you should now see the elixir console open up. Use CTRL+C to exit"
 
 iex
